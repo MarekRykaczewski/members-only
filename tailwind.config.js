73c819918e76e0091fc6f0c53cd5c625e627f4b7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-09 21:07:43 +0200

    add and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['views/**/*.pug'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
