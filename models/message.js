33f085f88210368f130ebcf1168d873c3b7604e4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-13 15:42:32 +0200

    increase textContent maxLength

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     title: { type: String, required: true, maxLength: 100 },
-    textContent: { type: String, required: true, maxLength: 100 },
+    textContent: { type: String, required: true, maxLength: 300 },
     createdAt: { type: Date, required: true, default: Date.now },
     owner: [{type: Schema.Types.ObjectId, ref: "User"}],
   });
